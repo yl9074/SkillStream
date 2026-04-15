435abf8ea0942fa7df69e8e1f074141f75c3a68f
yl <yuxuan.lew88@gmail.com>
2026-04-15 10:02:14 +0800

    Restarted project with fresh Django and venv setup

--- playground/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def say_hello(request):
-    # 用 render 工具，直接把刚才写好的 hello.html 渲染并发送给用户
-    return render(request, 'hello.html', {'name': 'Zheng Yang'})
