b38ea19d299687f960093432766d832c75d11b2a
yl <yuxuan.lew88@gmail.com>
2026-04-23 00:20:57 +0800

    Implement user registration and authentication views

--- skillstream/settings.py
+++ skillstream/settings.py
@@ -123,3 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Authentication Redirects
+LOGIN_REDIRECT_URL = 'catalog'
+LOGOUT_REDIRECT_URL = 'login'
