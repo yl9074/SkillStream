099c62cdaf50f0698010567342d024c91e5227e2
yl <yuxuan.lew88@gmail.com>
2026-06-12 01:51:36 +0800

    added function that rejects duplicate emails.

--- courses/forms.py
+++ courses/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -8,3 +9,11 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = User
         fields = UserCreationForm.Meta.fields + ('email',)
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        
+        if User.objects.filter(email=email).exists():
+            raise ValidationError("An account with this email address already exists. Please log in or use a different email.")
+            
+        return email    
